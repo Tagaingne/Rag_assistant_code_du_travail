6f2516dcdcf0c014c240da693f98f2a82cb25f63
BRUNO TAGAINGNE <btagaingne@gmail.com>
2026-07-09 09:58:33 +0200

    feat: ajout configuration et constantes

---
+++ src/config.py
@@ -0,0 +1,22 @@
+# src/config.py
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+try:
+    GROQ_API_KEY = os.environ["GROQ_API_KEY"]
+except KeyError:
+    raise Exception("La variable GROQ_API_KEY est absente du fichier .env")
+
+# Modèles
+LLM_MODEL = "llama-3.3-70b-versatile"
+EMBEDDING_MODEL = "distiluse-base-multilingual-cased-v2"
+MODERATOR_MODEL = "llama-3.1-8b-instant"
+
+# Base vectorielle
+VECTOR_DB_PATH = "vector_db"
+
+# Nombre de chunks à récupérer
+TOP_K = 5
